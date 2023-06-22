a10d0725883d26f7ecb53a74dfa71683a56109b3
hocinebouarara <hocinbouarara08@gmail.com>
2023-06-22 23:36:41 +0100

    start the project with it dependencies

---
+++ src/main/java/com/hocinebouarara/StudentMasterDegreeSpecialtyApplication/StudentMasterDegreeSpecialtyApplication.java
@@ -0,0 +1,13 @@
+package com.hocinebouarara.StudentMasterDegreeSpecialtyApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMasterDegreeSpecialtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMasterDegreeSpecialtyApplication.class, args);
+	}
+
+}

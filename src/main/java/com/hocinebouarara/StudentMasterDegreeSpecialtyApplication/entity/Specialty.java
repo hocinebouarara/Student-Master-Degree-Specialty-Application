f0bfef386c9c68894965ad1cb2727bf2b90d78f2
hocinebouarara <hocinbouarara08@gmail.com>
2023-06-23 14:53:59 +0100

    create the Specialty entity for mapping the specialties table in the database

---
+++ src/main/java/com/hocinebouarara/StudentMasterDegreeSpecialtyApplication/entity/Specialty.java
@@ -0,0 +1,35 @@
+package com.hocinebouarara.StudentMasterDegreeSpecialtyApplication.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "specialties")
+public class Specialty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "specialty_id")
+    private Long id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "available_places",nullable = false)
+    private int availablePlaces;
+
+    @OneToMany(mappedBy = "specialty")
+    private List<StudentChooseSpecialties> studentChooseSpecialties;
+
+
+}
+
